667a704892f62dd7a6ddff4b11a1debecadca8c4
Kevin Estrada <125918656+KevEstr@users.noreply.github.com>
2025-10-12 17:53:52 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -3,18 +3,24 @@ package parabank
 import scala.util.Random
 
 object Data {
+  
   val url = "https://parabank.parasoft.com/parabank/services/bank"
   val username = "john"
   val password = "demo"
   
+  // Cuentas existentes
   val accountIds = List(19671, 19782, 19893, 20004, 20115)
+  
+  // Customer IDs
+  val validCustomerIds = List(18206, 18317, 18428, 18539, 18650)
   
   val accountFeeder = Iterator.continually(Map(
     "accountId" -> accountIds(Random.nextInt(accountIds.length))
   ))
   
+  // Feeder para solicitudes de préstamos
   val loanFeeder = Iterator.continually(Map(
-    "customerId" -> (12000 + Random.nextInt(500)).toString,
+    "customerId" -> validCustomerIds(Random.nextInt(validCustomerIds.length)).toString,
     "loanAmount" -> (1000 + Random.nextInt(50000)),
     "downPayment" -> (100 + Random.nextInt(5000)),
     "fromAccountId" -> accountIds(Random.nextInt(accountIds.length))
