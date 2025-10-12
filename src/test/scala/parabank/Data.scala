356608f0d7302e3bda15aba27d6f9614b670f207
Kevin Estrada <125918656+KevEstr@users.noreply.github.com>
2025-10-12 15:06:33 -0500

    Update Data.scala

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -9,8 +9,14 @@ object Data {
   
   val accountIds = List(19671, 19782, 19893, 20004, 20115)
   
-  // HU3
   val accountFeeder = Iterator.continually(Map(
     "accountId" -> accountIds(Random.nextInt(accountIds.length))
   ))
-  }
+  
+  val loanFeeder = Iterator.continually(Map(
+    "customerId" -> (12000 + Random.nextInt(500)).toString,
+    "loanAmount" -> (1000 + Random.nextInt(50000)),
+    "downPayment" -> (100 + Random.nextInt(5000)),
+    "fromAccountId" -> accountIds(Random.nextInt(accountIds.length))
+  ))
+}
