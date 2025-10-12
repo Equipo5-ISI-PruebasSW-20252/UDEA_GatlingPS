050bdb435e1de701f6fd5631076381c01cc8aebf
Kevin Estrada <125918656+KevEstr@users.noreply.github.com>
2025-10-12 15:01:19 -0500

    Enhance Data object with account IDs and feeder
    Added account IDs and account feeder for random selection.

--- src/test/scala/parabank/Data.scala
+++ src/test/scala/parabank/Data.scala
@@ -1,7 +1,16 @@
 package parabank
 
-object Data{
-    val url= "https://parabank.parasoft.com/parabank/services/bank"
-    val username = "john"
-    val password = "demo"
-}
+import scala.util.Random
+
+object Data {
+  val url = "https://parabank.parasoft.com/parabank/services/bank"
+  val username = "john"
+  val password = "demo"
+  
+  val accountIds = List(19671, 19782, 19893, 20004, 20115)
+  
+  // HU3
+  val accountFeeder = Iterator.continually(Map(
+    "accountId" -> accountIds(Random.nextInt(accountIds.length))
+  ))
+  }
